fbc7b8ee4542f7e843c7fdbd01ee1fda493049f5
Fernando Nerio Jr <91402801+JrNerio30@users.noreply.github.com>
2024-01-26 22:02:56 -0700

    Added Cursor Trail Effect's functionality

--- script.js
+++ script.js
@@ -38,7 +38,20 @@ function drawmeatree(height){
 let treeheight = 10;
 drawmeatree(treeheight);
 
+// Cursor Trail Effect
+const cursorRing = document.getElementById("cursor-ring")
+    document.addEventListener("mousemove", (e) =>{
+        requestAnimationFrame(() =>{
+            cursorRing.style.left = `${e.clientX}px`;
+            cursorRing.style.top = `${e.clientY}px`;
+        });
+    });
 
+const toggleCursor = () => {
+    cursorRing.classList.toggle("active");
+    document.addEventListener("mousedown", toggleCursor)
+    document.addEventListener("mouseup", toggleCursor)
+}
 
     
 

d5a291ea36415dfa4ab50b1b9a4376cdcd6ddf1c
Fernando Nerio Jr <91402801+JrNerio30@users.noreply.github.com>
2024-02-01 23:03:22 -0700

    Deleted Files Due to Renamed Folder from 'True or False Calculator' to 'CSS Grid Practise'.

--- True or False Calculator/ToF.js
+++
@@ -1,9 +0,0 @@
-// Changes Navigation Header Background
-const scroll = document.getElementById("navigation")
-    document.addEventListener("scroll", () => {
-        if(window.scrollY > 0){
-            scroll.classList.add("scrolled")
-        }else{
-            scroll.classList.remove("scrolled")
-        }
-    });

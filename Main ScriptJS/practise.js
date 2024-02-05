76a3fdc4ee907aa87c4fd41f3c98bd9cf0716712
Fernando Nerio Jr <91402801+JrNerio30@users.noreply.github.com>
2024-02-04 23:12:16 -0700

    test: JavaScript practise

--- Main ScriptJS/practise.js
+++ Main ScriptJS/practise.js
@@ -75,3 +75,56 @@ console.log(mood)
 // Multiple Bindings.
 let luigisDebt = 150, mariosDebt = 250, Arthur = 200, paidbyLuck = 600
 console.log(luigisDebt + mariosDebt + Arthur - paidbyLuck)
+
+// The "let, const and var" are called bindings
+// const: "contstant" and var: "variable"
+// "let" can create multiple bindings and can be reassigned 
+// ex: line 65-69
+// but const is a "constant binding" and connat reassign another value. 
+// var/variable is for old websites. it is similar to let and const but can be very hard to read.
+
+// Return Values with Math max and min
+
+let valueS = 100
+console.log(Math.min(10, 20) + valueS)
+
+// Control Flow: When your program contains multiple statements, they are executed as if they a story, from the beggining to the end. 
+
+// let theNumber = Number(prompt("Pick a number"));
+// console.log("Your number is the square root of " + theNumber * theNumber);
+
+// Conditional Execution: A Branching Road. Whenever you start with one situation, it'll beocome more than one. 
+// Conditional Execution is created with the "if" statement/keyword in JavaScript. 
+
+// theNumber = Number(prompt("Pick a number"));
+//   if(!Number.isNaN(theNumber)){
+//     console.log("Your number is the square root of " + theNumber * theNumber);
+//   }
+
+if(1 + 1 == 2)console.log("It's True")
+// You can use "else" statement/keyword to create two separate, alternative execution paths. 
+// Ex #1:
+// let theNumber = Number(prompt("Pick a number"));
+// if(!Number.isNaN(theNumber)){
+//     console.log("You number is a square root of " + theNumber * theNumber);
+// }else{
+//     console.log("Hey. Why didn't you give me a number?");
+// }
+// Ex #2:
+const MichealMoney = 30, JamesMoney = 100
+if(MichealMoney > JamesMoney){
+    console.log("Micheal is broke");
+}else{
+    console.log("James is richer than Micheal and Micheal broke asf.")
+}
+
+// if you have more than two paths to choose from, you can "chain" using multiple if else or else if together.
+// Ex:1
+let yourMoney = Number(prompt("How much money you got?"))
+if(yourMoney < 100){
+    alert("YOU IS BROKE BROKE")
+}else if(yourMoney < 1000){
+    alert("YOUR'RE BROKE ASF")
+}else{
+    alert("YOUR'RE RICH RICH")
+}
